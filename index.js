a53fdeef68231dd6ceb49806e940c634896de44f
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-10-25 11:53:23 +0600

    categories.json add

--- index.js
+++ index.js
@@ -3,9 +3,20 @@ const app = express()
 const port = 5000
 const cors = require('cors')
 
-
 app.use(cors());
 
+const categories = require('./data/categories.json');
+console.log(categories);
+
+app.get('/course-categories', (req, res) => {
+    res.send(categories);
+})
+
+app.get('/category/:id', (req, res) => {
+    const id = req.params.id;
+    const category_course = news.filter(n => n.category_id === id);
+    res.send(category_course);
+})
 
 app.get('/', (req, res) => {
     res.send('Server is running')
