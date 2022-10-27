a4c3836f02393fa5786edb50e74efa40695cfd2e
ShakilTalukder07 <shakil.talukder07@gmail.com>
2022-10-27 23:01:38 +0600

    done

--- index.js
+++ index.js
@@ -1,36 +1,46 @@
-const express = require('express')
-const app = express()
-const port = 5000
-const cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require("cors");
+const port = process.env.PORT || 5000;
 
 app.use(cors());
 
-const categories = require('./data/categories.json');
-const course = require('./data/course.json');
+const categories = require("./data/category.json");
+const courses = require("./data/courses.json");
+const question = require("./data/question.json");
 
-app.get('/course-categories', (req, res) => {
-    res.send(categories);
-})
+app.get("/", (req, res) => {
+  res.send("Server is ready to fight");
+});
 
-app.get('/category/:id', (req, res) => {
-    const id = req.params.id;
-    const categoryCourse = news.filter(n => n.category_id === id);
-    res.send(categoryCourse);
-})
+app.get("/course-category", (req, res) => {
+  res.send(categories);
+});
 
-app.get('/course', (req, res) => {
-    res.send(course);
-})
+app.get("/category/:id", (req, res) => {
+  const id = req.params.id;
+  const category_course = courses.filter((c) => c.category_id === id);
+  res.send(category_course);
+});
 
-app.get('/course/:id', (req, res) => {
-    const id = req.params.id;
-    const selectedCourse = news.filter(s => s._id === id);
-    res.send(selectedCourse)
-})
+app.get("/courses", (req, res) => {
+  res.send(courses);
+});
 
-app.get('/', (req, res) => {
-    res.send('Server is running')
-})
+app.get("/courses/:id", (req, res) => {
+  const id = req.params.id;
+  const course = courses.find((c) => c._id === id);
+  res.send(course);
+});
+
+app.get("/questions", (req, res) => {
+  res.send(question);
+});
+
+app.get("/faq", (req, res) => {
+  res.send(question);
+});
+
 app.listen(port, () => {
-    console.log(`Example app listening on port ${port}`)
-})
+  console.log("port is running", port);
+});
